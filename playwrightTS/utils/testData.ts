6e4cc9c2756bb2641960f97449ba906b4e768697
zaki samson <zakisamson@gmail.com>
2026-06-30 21:40:51 +0700

    added test cases

---
+++ playwrightTS/utils/testData.ts
@@ -0,0 +1,26 @@
+export const loginData = {
+    standardUser: {
+        username: 'standard_user',
+        password: 'secret_sauce'
+    },
+    lockedOutUser: {
+        username: 'locked_out_user',
+        password: 'secret_sauce'
+    },
+    problemUser: {
+        username: 'problem_user',
+        password: 'secret_sauce'
+    },
+    performanceGlitchUser: {
+        username: 'performance_glitch_user',
+        password: 'secret_sauce'
+    },
+    errorUser: {
+        username: 'error_user',
+        password: 'secret_sauce'
+    },
+    visualUser: {
+        username: 'visual_user',
+        password: 'secret_sauce'
+    },
+}
